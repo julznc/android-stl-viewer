f3470a7dc407a48534616e7555ab7c7e71b802a3
yus <yus.projectproto@gmail.com>
2015-07-19 16:50:17 +0800

    projection & camera transformation

--- app/src/main/java/com/ymsoftlabs/stlviewer/GLRenderer.java
+++ app/src/main/java/com/ymsoftlabs/stlviewer/GLRenderer.java
@@ -2,11 +2,17 @@ package com.ymsoftlabs.stlviewer;
 
 import android.opengl.GLES20;
 import android.opengl.GLSurfaceView;
+import android.opengl.Matrix;
 
 import javax.microedition.khronos.egl.EGLConfig;
 import javax.microedition.khronos.opengles.GL10;
 
 public class GLRenderer implements GLSurfaceView.Renderer {
+
+    // mMVPMatrix is an abbreviation for "Model View Projection Matrix"
+    private final float[] mMVPMatrix = new float[16];
+    private final float[] mProjectionMatrix = new float[16];
+    private final float[] mViewMatrix = new float[16];
 
     private Triangle mTriangle;
     private Square mSquare;
@@ -23,6 +29,10 @@ public class GLRenderer implements GLSurfaceView.Renderer {
     @Override
     public void onSurfaceChanged(GL10 gl, int width, int height) {
         GLES20.glViewport(0, 0, width, height);
+
+        float ratio = (float) width / height;
+
+        Matrix.frustumM(mProjectionMatrix, 0, -ratio, ratio, -1, 1, 3, 7);
     }
 
     @Override
@@ -30,7 +40,13 @@ public class GLRenderer implements GLSurfaceView.Renderer {
         // redraw background color
         GLES20.glClear(GLES20.GL_COLOR_BUFFER_BIT);
 
-        mTriangle.draw();
+        // camera position
+        Matrix.setLookAtM(mViewMatrix, 0, 0, 0, -3, 0f, 0f, 0f, 0f, 1.0f, 0.0f);
+
+        // projection & view transformaation
+        Matrix.multiplyMM(mMVPMatrix, 0, mProjectionMatrix, 0, mViewMatrix, 0);
+
+        mTriangle.draw(mMVPMatrix);
     }
 
     public static int loadShader(int type, String shaderCode) {
--- app/src/main/java/com/ymsoftlabs/stlviewer/Triangle.java
+++ app/src/main/java/com/ymsoftlabs/stlviewer/Triangle.java
@@ -10,9 +10,10 @@ public class Triangle {
     private FloatBuffer vertexBuffer;
 
     private final String vertexShaderCode =
+            "uniform mat4 uMVPMatrix;" +
             "attribute vec4 vPosition;" +
             "void main() {" +
-            "  gl_Position = vPosition;" +
+            "  gl_Position = uMVPMatrix * vPosition;" +
             "}";
     private final String fragmentShaderCode =
             "precision mediump float;" +
@@ -20,6 +21,8 @@ public class Triangle {
             "void main() {" +
             "  gl_FragColor = vColor;" +
             "}";
+
+    private int mMVPMatrixHandle;
 
     // number of coordinates per vertex in this array
     static final int COORDS_PER_VERTEX = 3;
@@ -58,7 +61,7 @@ public class Triangle {
         GLES20.glLinkProgram(mProgram);
     }
 
-    public void draw() {
+    public void draw(float[] mvpMatrix) {
         GLES20.glUseProgram(mProgram);
 
         mPositionHandle = GLES20.glGetAttribLocation(mProgram, "vPosition");
@@ -69,6 +72,11 @@ public class Triangle {
 
         mColorHandle = GLES20.glGetUniformLocation(mProgram, "vColor");
         GLES20.glUniform4fv(mColorHandle, 1, color, 0);
+
+        mMVPMatrixHandle = GLES20.glGetUniformLocation(mProgram, "uMVPMatrix");
+
+        GLES20.glUniformMatrix4fv(mMVPMatrixHandle, 1, false, mvpMatrix, 0);
+
         GLES20.glDrawArrays(GLES20.GL_TRIANGLES, 0, vertexCount);
 
         GLES20.glDisableVertexAttribArray(mPositionHandle);
