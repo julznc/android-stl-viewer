6055104b5af5b71550180405f79e5ea5dc409e51
yus <yus.projectproto@gmail.com>
2015-07-19 15:01:26 +0800

    draw triangle

--- app/src/main/java/com/ymsoftlabs/stlviewer/GLRenderer.java
+++ app/src/main/java/com/ymsoftlabs/stlviewer/GLRenderer.java
@@ -7,10 +7,17 @@ import javax.microedition.khronos.egl.EGLConfig;
 import javax.microedition.khronos.opengles.GL10;
 
 public class GLRenderer implements GLSurfaceView.Renderer {
+
+    private Triangle mTriangle;
+    private Square mSquare;
+
     @Override
     public void onSurfaceCreated(GL10 gl, EGLConfig config) {
         // background frame color
         GLES20.glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+
+        mTriangle = new Triangle();
+        mSquare = new Square();
     }
 
     @Override
@@ -22,5 +29,14 @@ public class GLRenderer implements GLSurfaceView.Renderer {
     public void onDrawFrame(GL10 gl) {
         // redraw background color
         GLES20.glClear(GLES20.GL_COLOR_BUFFER_BIT);
+
+        mTriangle.draw();
+    }
+
+    public static int loadShader(int type, String shaderCode) {
+        int shader = GLES20.glCreateShader(type);
+        GLES20.glShaderSource(shader, shaderCode);
+        GLES20.glCompileShader(shader);
+        return shader;
     }
 }
--- app/src/main/java/com/ymsoftlabs/stlviewer/Triangle.java
+++ app/src/main/java/com/ymsoftlabs/stlviewer/Triangle.java
@@ -1,11 +1,25 @@
 package com.ymsoftlabs.stlviewer;
 
+import android.opengl.GLES20;
+
 import java.nio.ByteBuffer;
 import java.nio.ByteOrder;
 import java.nio.FloatBuffer;
 
 public class Triangle {
     private FloatBuffer vertexBuffer;
+
+    private final String vertexShaderCode =
+            "attribute vec4 vPosition;" +
+            "void main() {" +
+            "  gl_Position = vPosition;" +
+            "}";
+    private final String fragmentShaderCode =
+            "precision mediump float;" +
+            "uniform vec4 vColor;" +
+            "void main() {" +
+            "  gl_FragColor = vColor;" +
+            "}";
 
     // number of coordinates per vertex in this array
     static final int COORDS_PER_VERTEX = 3;
@@ -18,6 +32,13 @@ public class Triangle {
     // rgba color
     float color[] = { 0.63671875f, 0.76953125f, 0.22265625f, 1.0f };
 
+    private final int mProgram;
+    private int mPositionHandle;
+    private int mColorHandle;
+
+    private final int vertexCount = triangleCoords.length / COORDS_PER_VERTEX;
+    private final int vertexStride = COORDS_PER_VERTEX * 4; // x4 bytes
+
     public Triangle() {
         // vertex byte buffer for shape coordinates // 4bytes per float
         ByteBuffer bb = ByteBuffer.allocateDirect(triangleCoords.length * 4);
@@ -26,5 +47,30 @@ public class Triangle {
         vertexBuffer = bb.asFloatBuffer(); // float buffer from byte buffer
         vertexBuffer.put(triangleCoords); // add the coordinates to the buffer
         vertexBuffer.position(0); // first coordinate
+
+        int vertexShader = GLRenderer.loadShader(GLES20.GL_VERTEX_SHADER, vertexShaderCode);
+        int fragmentShader = GLRenderer.loadShader(GLES20.GL_FRAGMENT_SHADER, fragmentShaderCode);
+
+        // create empty OpenGL ES Program
+        mProgram = GLES20.glCreateProgram();
+        GLES20.glAttachShader(mProgram, vertexShader);
+        GLES20.glAttachShader(mProgram, fragmentShader);
+        GLES20.glLinkProgram(mProgram);
+    }
+
+    public void draw() {
+        GLES20.glUseProgram(mProgram);
+
+        mPositionHandle = GLES20.glGetAttribLocation(mProgram, "vPosition");
+        GLES20.glEnableVertexAttribArray(mPositionHandle);
+
+        GLES20.glVertexAttribPointer(mPositionHandle, COORDS_PER_VERTEX,
+                GLES20.GL_FLOAT, false, vertexStride, vertexBuffer);
+
+        mColorHandle = GLES20.glGetUniformLocation(mProgram, "vColor");
+        GLES20.glUniform4fv(mColorHandle, 1, color, 0);
+        GLES20.glDrawArrays(GLES20.GL_TRIANGLES, 0, vertexCount);
+
+        GLES20.glDisableVertexAttribArray(mPositionHandle);
     }
 }
