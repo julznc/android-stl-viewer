e0c18a40b1d9106d8363097d342ad4c47c9e80b1
yus <a.b@c>
2015-08-08 20:59:46 +0800

    remove min/mag filter settings

--- app/src/main/java/com/learnopengles/android/lesson6/LessonSixActivity.java
+++ app/src/main/java/com/learnopengles/android/lesson6/LessonSixActivity.java
@@ -2,16 +2,10 @@ package com.learnopengles.android.lesson6;
 
 import android.app.Activity;
 import android.app.ActivityManager;
-import android.app.AlertDialog;
-import android.app.Dialog;
 import android.content.Context;
-import android.content.DialogInterface;
 import android.content.pm.ConfigurationInfo;
-import android.opengl.GLES20;
 import android.os.Bundle;
 import android.util.DisplayMetrics;
-import android.view.View;
-import android.view.View.OnClickListener;
 
 import com.learnopengles.android.R;
 
@@ -23,13 +17,7 @@ public class LessonSixActivity extends Activity
 	
 	private static final int MIN_DIALOG = 1;
 	private static final int MAG_DIALOG = 2;
-	
-	private int mMinSetting = -1;
-	private int mMagSetting = -1;
-	
-	private static final String MIN_SETTING = "min_setting";
-	private static final String MAG_SETTING = "mag_setting";
-	
+
 	@Override
 	public void onCreate(Bundle savedInstanceState) 
 	{
@@ -62,34 +50,6 @@ public class LessonSixActivity extends Activity
 			// renderer if you wanted to support both ES 1 and ES 2.
 			return;
 		}
-		
-		findViewById(R.id.button_set_min_filter).setOnClickListener(new OnClickListener() 
-		{		
-			@Override
-			public void onClick(View v) 
-			{
-				showDialog(MIN_DIALOG);				
-			}
-		});
-
-		findViewById(R.id.button_set_mag_filter).setOnClickListener(new OnClickListener() 
-		{		
-			@Override
-			public void onClick(View v) 
-			{
-				showDialog(MAG_DIALOG);				
-			}
-		});
-		
-		// Restore previous settings
-		if (savedInstanceState != null)
-		{
-			mMinSetting = savedInstanceState.getInt(MIN_SETTING, -1);
-			mMagSetting = savedInstanceState.getInt(MAG_SETTING, -1);
-			
-			if (mMinSetting != -1) { setMinSetting(mMinSetting); }
-			if (mMagSetting != -1) { setMagSetting(mMagSetting); }
-		}
 	}
 
 	@Override
@@ -108,124 +68,5 @@ public class LessonSixActivity extends Activity
 		// onPause().
 		super.onPause();
 		mGLSurfaceView.onPause();
-	}
-	
-	@Override
-	protected void onSaveInstanceState (Bundle outState)
-	{
-		outState.putInt(MIN_SETTING, mMinSetting);
-		outState.putInt(MAG_SETTING, mMagSetting);
-	}
-	
-	private void setMinSetting(final int item)
-	{
-		mMinSetting = item;
-		
-		mGLSurfaceView.queueEvent(new Runnable()
-		{
-			@Override
-			public void run()
-			{
-				final int filter;
-				
-				if (item == 0)
-				{
-					filter = GLES20.GL_NEAREST;
-				}
-				else if (item == 1)
-				{
-					filter = GLES20.GL_LINEAR;
-				}
-				else if (item == 2)
-				{
-					filter = GLES20.GL_NEAREST_MIPMAP_NEAREST;
-				}
-				else if (item == 3)
-				{
-					filter = GLES20.GL_NEAREST_MIPMAP_LINEAR;
-				}
-				else if (item == 4)
-				{
-					filter = GLES20.GL_LINEAR_MIPMAP_NEAREST;
-				}
-				else // if (item == 5)
-				{
-					filter = GLES20.GL_LINEAR_MIPMAP_LINEAR;
-				}
-				
-				mRenderer.setMinFilter(filter);
-			}
-		});
-	}
-	
-	private void setMagSetting(final int item)
-	{
-		mMagSetting = item;
-		
-		mGLSurfaceView.queueEvent(new Runnable()
-		{
-			@Override
-			public void run()
-			{
-				final int filter;
-				
-				if (item == 0)
-				{
-					filter = GLES20.GL_NEAREST;
-				}
-				else // if (item == 1)
-				{
-					filter = GLES20.GL_LINEAR;
-				}	    						
-				
-				mRenderer.setMagFilter(filter);
-			}
-		});
-	}
-	
-	@Override
-	protected Dialog onCreateDialog(int id) 
-	{
-	    Dialog dialog = null;
-	    
-	    switch(id) 
-	    {
-	    	case MIN_DIALOG:
-	    	{
-	    		AlertDialog.Builder builder = new AlertDialog.Builder(this);
-	    		builder.setTitle(getText(R.string.lesson_six_set_min_filter_message));
-	    		builder.setItems(getResources().getStringArray(R.array.lesson_six_min_filter_types), new DialogInterface.OnClickListener() 	    		
-	    		{
-	    			@Override
-	    		    public void onClick(final DialogInterface dialog, final int item) 
-	    			{
-	    				setMinSetting(item);
-	    		    }
-	    		});
-	    		
-	    		dialog = builder.create();
-	    	}
-	        break;
-	    	case MAG_DIALOG:
-	    	{
-	    		AlertDialog.Builder builder = new AlertDialog.Builder(this);
-	    		builder.setTitle(getText(R.string.lesson_six_set_mag_filter_message));
-	    		builder.setItems(getResources().getStringArray(R.array.lesson_six_mag_filter_types), new DialogInterface.OnClickListener() 	    		
-	    		{
-	    			@Override
-	    		    public void onClick(final DialogInterface dialog, final int item) 
-	    			{
-	    				setMagSetting(item);
-	    		    }
-	    		});
-	    		
-	    		dialog = builder.create();
-	    	}
-	        break;
-	    default:
-	        dialog = null;
-	    }
-	    
-	    return dialog;
 	}
 }
